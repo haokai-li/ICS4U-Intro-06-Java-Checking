900d06a84f79889112e3ed4e37a540d8398fb25d
Ubuntu <ubuntu@ip-172-31-82-118.ec2.internal>
2021-11-18 17:19:24 +0000

    hello world 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,16 +1,39 @@
-/**
- * The HelloWorld program implements an application that
- * simply displays "Hello World!" to the standard output.
- *
- * @author  Haokai
- * @version 1.0
- * @since   2021-11-17
- */
-class HelloWorld {
+/*
+* The "Hello Word!" program, just proves you can show print to terminal.
+*
+* @author  Haokai
+* @version 1.0
+* @since   2021-11-17
+*/
 
-    public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+/**
+* This is the standard "Hello, World!" program.
+*/
+final class HelloWorld {
+
+    /**
+    * Prevent instantiation.
+    * Throw an exception IllegalStateException.
+    * if this ever is called
+    *
+    * @throws IllegalStateException
+    *
+    */
+    private HelloWorld() {
+        throw new IllegalStateException("Cannot be instantiated");
     }
 
+    /**
+    * The starting main() function.
+    *
+    * @param args No args will be used
+    */
+    public static void main(final String[] args) {
+
+        // print out "Hello, World!"
+        System.out.println("Hello, World!");
+
+        System.out.println("\nDone.");
+    }
 }
+
